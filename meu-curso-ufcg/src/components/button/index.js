fd4fa86060f990521b2065a7393f52bbb4576f97
gabrielvba <gabriel.alves@ccc.ufcg.edu.br>
2022-03-15 14:39:14 -0300

    Criação do componente BaseButton

--- meu-curso-ufcg/src/components/button/index.js
+++ meu-curso-ufcg/src/components/button/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './base.css';
 
 function BaseButton(props) {
-  const { danger = false, onClick, children, styles } = props;
+  const { danger, profile = false, onClick, children, styles } = props;
 
-  let className = 'component-button-base-button';
+  let className = profile ? 'component-button-base-profile' : 'component-button-base-button';
 
-  className += danger ? ' component-button-base-button' : ' component-button-base-button-color';
+  className += danger ? ' component-button-base-danger' : ' component-button-base-button-color';
 
   return (
     <button type="button" className={className} onClick={onClick} style={styles}>
